bd8f8edc3fe593bf757fe4d3c8b69d4771fe9892
Marco Del Rosario <marsdel@espol.edu.ec>
2021-08-24 20:49:57 -0500

    carga de la base de datos y push de datos quemados

--- app/app.py
+++ app/app.py
@@ -1,6 +1,37 @@
-from flask import Flask
-app = Flask(__name__)
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+#cargando certificado del proyecto firebase
+firebase_sdk = credentials.Certificate('pruebaslp-19813-firebase-adminsdk-2l6b4-36850be328.json')
+
+#referencia al database en tiempo real de firebase
+firebase_admin.initialize_app(firebase_sdk,{'databaseURL':'https://pruebaslp-19813-default-rtdb.firebaseio.com/'})
+
+'''users = db.reference('/users')
+users.push({
+    'nombre': 'david',
+    'apellido': 'yanez',
+    'clave': '123456',
+    'correo': 'dfyanez@espol.edu.ec',
+    'tipo': 'propietario',
+})'''
+
+'''local = db.reference('/local')
+local.push(
+    {
+        'nombre': 'PastelesChino',
+        'apertura': '08:00',
+        'cierre': '16:00',
+        'direccion': 'ESPOL',
+        'tipo': 'restaurante',
+        'capacidad': 60,
+        'propietario': 'hector',
+        'contacto': ['0989816645','0993386084'],
+    }
+)'''
+
+#obtener datos
+ref = db.reference('users')
+datos = ref.child('-MhuoF8tSDPhTYQedOAS')
+print(datos.get())
